d62e17d8a59e740dcb224045f37760e74b39b366
Megan Horsfield <meganthadev@gmail.com>
2020-02-17 04:17:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+badges = ["Hello, my name is #{name}."]
+room_assignments = [1, 2, 3, 4, 5, 6, 7]
+
+
+def badge_maker(name)
+puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  return badge_messsages
+end
+
+def assign_rooms(speakers)
+  
+end 
+  
