67dcdb2e2dc4b0b8d5314dc42bd6d9b257308c95
Megan Horsfield <meganthadev@gmail.com>
2020-02-17 04:20:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,5 @@
 # Write your code here.
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-badges = ["Hello, my name is #{attendees}."]
 room_assignments = [1, 2, 3, 4, 5, 6, 7]
 
 
